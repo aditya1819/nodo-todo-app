3715ef13c78bc46477ced7f7f29f3400a97421d2
Aditya Shahare <shahare.aditya@gmail.com>
2023-03-27 18:06:08 +0530

    Added basic node todo app

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+var port = process.env.PORT || 3000;
+
+let items = ['Buy Food'];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  var today = new Date();
+
+  let options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long'
+  };
+
+  let day = today.toLocaleDateString('en-US', options);
+
+  res.render('list', { kindOfDay: day, newListItems: items });
+
+  app.post('/', (req, res) => {
+    let item = req.body.newItem;
+
+    items.push(item);
+
+    res.redirect('/');
+  });
+});
+
+app.listen(port, function () {
+  console.log('Server running on port : ' + port);
+});
